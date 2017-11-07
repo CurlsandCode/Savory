9c8b5e373520f418b86a3c5c8ac32d1c72ddbb99
Monika <monika.m.williams@gmail.com>
2017-11-07 18:37:02 +0900

    added ingredients model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106123151) do
+ActiveRecord::Schema.define(version: 20171107092544) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
