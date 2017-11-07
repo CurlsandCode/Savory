ca444d99cce136620b72b84981d141546a0e218b
Monika <monika.m.williams@gmail.com>
2017-11-07 19:40:52 +0900

    built out show action and before_action method, also modified recipes index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,20 @@
 class RecipesController < ApplicationController
+before_action :set_recipe ,only: [:show,:edit,:update,:destroy]
 	def index
-		@recipe = Recipe.all
+		@recipes = Recipe.all
 	end
 	def show
 	end
 	def new
+		@
 	end
 	def create
+	end
+	
+	private
+	
+	def set_recipe
+		@recipe = Recipe.find(params[:id])
 	end
 	
 end
