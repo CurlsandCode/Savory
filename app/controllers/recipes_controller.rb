d0a0da9f936303dd2b76df27c293e5795e895ee2
Monika <monika.m.williams@gmail.com>
2017-11-10 21:27:23 +0900

    updated recipe controller and index, layouts,and gemfile

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
 before_action :set_recipe ,only: [:show,:edit,:update,:destroy]
 	
 	def index
-		@recipes = Recipe.all
+		@recipe = Recipe.all
 	end
 	def show
 	end
