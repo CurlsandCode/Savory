16532690941b30a2fd94b8e86f6fe74085de2ce1
Monika <monika.m.williams@gmail.com>
2017-11-15 21:21:59 +0900

    corrected a misspelling in recipe_ingredients table

---
+++ db/migrate/20171115114511_rename_ingredients_id_to_ingredient_id.rb
@@ -0,0 +1,5 @@
+class RenameIngredientsIdToIngredientId < ActiveRecord::Migration[5.1]
+   def change
+    rename_column :recipe_ingredients, :ingredients_id, :ingredient_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112055129) do
+ActiveRecord::Schema.define(version: 20171115114511) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171112055129) do
 
   create_table "recipe_ingredients", force: :cascade do |t|
     t.integer "recipe_id"
-    t.integer "ingredients_id"
+    t.integer "ingredient_id"
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
