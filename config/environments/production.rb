2d0e021453609dd2c2822b6ccbe1210f331d71e9
Monika <monika.m.williams@gmail.com>
2017-11-20 20:54:02 +0900

    adding s3 host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
       },
-    :url =>':savory-site.s3.amazonaws.com',
-    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_host_name =>'savory-site.s3.amazonaws.com'
+    
     } 
 end
