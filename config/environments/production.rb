fce3a02ed20a4798d48ef1a6f8e4cd00bb3a2a71
Monika <monika.m.williams@gmail.com>
2017-11-20 05:40:21 -0500

    attempting to get aws to working with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-	
+	#paperclip configs
 	config.paperclip_defaults = {
      storage: :s3,
      s3_credentials: {
