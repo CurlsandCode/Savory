1ae6f57c99a99d5871ce286890048f8e40ed65bf
Monika <monika.m.williams@gmail.com>
2017-11-22 21:52:12 +0900

    added nested resouce,so that you cook link to all recipes created by a user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 	resources :recipes
 	 root to: "home#index"
+	resources :users, only: [:show] do
+		resources :recipes, only: [:index]
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
