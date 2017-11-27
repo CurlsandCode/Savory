2724634d153bfddc0220449d885eb789ff209d6a
Monika <monika.m.williams@gmail.com>
2017-11-27 21:20:57 +0900

    cleaned up recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,57 +1,64 @@
 class RecipesController < ApplicationController
-before_action :set_recipe ,only: [:show,:edit,:update,:destroy]
-	
-	def index
-		if params[:user_id]
-		 @recipes = User.find(params[:user_id]).recipes
-		else
-	     @recipes = Recipe.all
+		before_action :set_recipe ,only: [:show,:edit,:update,:destroy]
+		
+		def index
+				@recipes = Recipe.all
+				@recent_recipes = Recipe.recently_added_recipes
+			if params[:user_id]
+				@recipes = User.find(params[:user_id]).recipes
+			end
+	 	end
+		
+		def show
 		end
-		 @recent_recipes = Recipe.recently_added_recipes
-	 end
-	def show
-	end
-	def new
-		if user_signed_in?
-		@recipe = Recipe.new
-		5.times {@recipe.recipe_ingredients.build.build_ingredient}
-		5.times  {@recipe.directions.build}
-		else 
-			redirect_to  new_user_session_path
-	  end
-	end
-	def create
-		@recipe = Recipe.create (recipe_params)
-		if @recipe.save
-		redirect_to @recipe , notice: "Successfully created new recipe"
-		else
-			render 'new'
+		
+		def new
+			if user_signed_in?
+				@recipe = Recipe.new
+				5.times {@recipe.recipe_ingredients.build.build_ingredient}
+				5.times  {@recipe.directions.build}
+			else 
+				redirect_to  new_user_session_path
+	  	end
 		end
-	end
-	def edit
+		
+		def create
+			@recipe = Recipe.create (recipe_params)
+			if @recipe.save
+				redirect_to @recipe , notice: "Successfully created new recipe"
+			else
+				5.times {@recipe.recipe_ingredients.build.build_ingredient}
+		    5.times  {@recipe.directions.build}
+				render 'new'
+			end
+		end
+		
+		def edit
      5.times { @recipe.recipe_ingredients.build.build_ingredient}
      5.times { @recipe.directions.build}
-	end
+		end
 	
-	def update
-         if @recipe.update(recipe_params)
-           redirect_to @recipe, notice: "Recipe successfully updated."
-         else
-          render :edit
-       end
-	end
-	def destroy
-		@recipe.destroy
-		redirect_to @recipe, notice: "Recipe successfully destroyed."
-	end
+		def update
+    	if @recipe.update(recipe_params)
+      	redirect_to @recipe, notice: "Recipe successfully updated."
+      else
+        render :edit
+      end
+	  end
+		
+		def destroy
+			@recipe.destroy
+			redirect_to @recipe, notice: "Recipe successfully destroyed."
+		end
 	
-	private
+		private
 	
-	def set_recipe
-		@recipe = Recipe.find(params[:id])
-	end
-	 def recipe_params
-    params.require(:recipe).permit(
+		def set_recipe
+			@recipe = Recipe.find(params[:id])
+		end
+		
+	 	def recipe_params
+    	params.require(:recipe).permit(
       :name,
       :description,
       :image,
