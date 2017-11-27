2daf31bdd37b5a1654c9890290a14ec821dae57b
Monika <monika.m.williams@gmail.com>
2017-11-27 20:18:19 +0900

    cleaned up partial form and recipe model, making them readable

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,15 +5,16 @@ class Recipe < ApplicationRecord
 	has_many :ingredients, through: :recipe_ingredients
 	validates :name, :description, :image, presence: true
 
+
 	accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attr| attr[:quantity].blank?}
 	accepts_nested_attributes_for :directions, :reject_if => proc { |attr| attr[:step].blank?}
 	
 	 has_attached_file :image, styles: { :medium => "400x400#"}
-     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-		
-    def self.recently_added_recipes
-	  Recipe.last(4).reverse
-	end
+   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+			
+			def self.recently_added_recipes
+	  		Recipe.last(4).reverse
+			end
 end
 		
 
