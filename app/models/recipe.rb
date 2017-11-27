f4658a9285d9b8d4f20d7721927a8b6e8918b467
Monika <monika.m.williams@gmail.com>
2017-11-27 22:56:19 +0900

    refactor directions accepts nested attributes into custom attribute writer and added more validations. And cleaned up ingredient and recipeingredient models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,17 +4,27 @@ class Recipe < ApplicationRecord
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
 	validates :name, :description, :image, presence: true
-
-
-	accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attr| attr[:quantity].blank?}
-	accepts_nested_attributes_for :directions, :reject_if => proc { |attr| attr[:step].blank?}
+  validates :name, uniqueness: true
+  validates :recipe_ingredients, presence: true
+	validates :directions, presence: true
 	
-	 has_attached_file :image, styles: { :medium => "400x400#"}
-   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attr| attr[:quantity].blank?}
+	
+	
+	has_attached_file :image, styles: { :medium => "400x400#"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 			
-			def self.recently_added_recipes
-	  		Recipe.last(4).reverse
-			end
-end
+	def self.recently_added_recipes
+	  Recipe.last(4).reverse
+	end
 		
+		
+  def directions_attributes=(directions_attributes)
+    directions_attributes.each do |i,direction_attr|
+      if direction_attr[:step].present?
+          self.directions.build(direction_attr)
+        end
+      end
+	 end
+ end
 
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,5 +1,6 @@
 class RecipeIngredient < ApplicationRecord
 	belongs_to :recipe, optional: true
 	belongs_to :ingredient, optional: true
-    accepts_nested_attributes_for :ingredient, :reject_if => proc { |attr| attr[:name].blank?}
+	accepts_nested_attributes_for :ingredient, :reject_if => proc { |attr| attr[:name].blank?}
+
 end
