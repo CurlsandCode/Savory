8cfc545dd03cc5bb1986124d0847bf2032247ffc
Monika <monika.m.williams@gmail.com>
2017-12-02 23:25:05 +0900

    added a custom attribute writer to recipe_ingredient model and removed the directions custom attribute writer from recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,12 +4,11 @@ class Recipe < ApplicationRecord
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
 	validates :name, :description, :image, presence: true
-  validates :name, uniqueness: true
+	validates :name, uniqueness: true
   validates :recipe_ingredients, presence: true
 	validates :directions, presence: true
-	
 	accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attr| attr[:quantity].blank?}
-	
+  accepts_nested_attributes_for :directions, :reject_if => proc { |attr| attr[:step].blank?}
 	
 	has_attached_file :image, styles: { :medium => "400x400#"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
@@ -19,12 +18,7 @@ class Recipe < ApplicationRecord
 	end
 		
 		
-  def directions_attributes=(directions_attributes)
-    directions_attributes.each do |i,direction_attr|
-      if direction_attr[:step].present?
-          self.directions.build(direction_attr)
-        end
-      end
-	 end
+
  end
+
 
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,14 @@
 class RecipeIngredient < ApplicationRecord
 	belongs_to :recipe, optional: true
 	belongs_to :ingredient, optional: true
-	accepts_nested_attributes_for :ingredient, :reject_if => proc { |attr| attr[:name].blank?}
 
+      
+  def ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.values.each do |attribute|
+      if attribute != ""
+        new_ingredient = Ingredient.find_or_create_by(name: attribute)
+        self.ingredient = new_ingredient
+      end
+    end
+  end
 end
