d11eb41f33cd7b0be35f88da213411ee4dc37819
Monika <monika.m.williams@gmail.com>
2017-12-04 21:49:59 +0900

    removed default image, didnt like how it looked

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,14 +3,14 @@ class Recipe < ApplicationRecord
 	has_many :directions
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
-	validates :name, :description, presence: true
+	validates :name, :description, :image, presence: true
 	validates :name, uniqueness: true
   validates :recipe_ingredients, presence: true
 	validates :directions, presence: true
 	accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attr| attr[:quantity].blank?}
   accepts_nested_attributes_for :directions, :reject_if => proc { |attr| attr[:step].blank?}
 	
-	has_attached_file :image, styles: { :medium => "400x400#"}, default_url: ":style/default.png"
+	has_attached_file :image, styles: { :medium => "400x400#"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 			
 	def self.recently_added_recipes
