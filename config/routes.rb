fb0d79cf773af934edcfc3d98b14cd9c9398f21e
Monika <monika.m.williams@gmail.com>
2017-12-25 20:49:59 +0900

    updated routes with comment resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
    
-  
+  resources :comments
 	  resources :recipes do
       member do
           put "like",    to: "recipes#upvote"
           put "dislike", to: "recipes#downvote"
       end
+			resources :comments
 		end
 	 root to: "home#index"
 	
