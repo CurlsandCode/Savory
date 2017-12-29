fb5eb8bdbf70b64ca792a39690d50ddfcea2ffa4
Monika <monika.m.williams@gmail.com>
2017-12-29 18:11:02 +0900

    added code to recipes.js file in order to try to get read more button working,currently broken, made changes to application and recipe views to try to get read more button working. delete code from recipes.coffee when I realized I was writing in the wrong file.

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,8 @@
+   $(".js-more").on('click', function(event){
+		event.preventDefault();
+		var id = $(this).data("id");
+		$.get("/recipes/" + id + ".json", function(data) {
+			$("#body-" + id).text(data["body"]);
+			});
+		});
+	
