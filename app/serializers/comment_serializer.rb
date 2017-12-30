c187e6330e25af0f154f8b67fe4f4f2fda8ab9b4
Monika <monika.m.williams@gmail.com>
2017-12-30 20:31:56 +0900

    added a method so that the username for each comment shows

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :created_at
+  attributes :id, :body, :created_at, :user
 	has_one :recipe
 	has_one :user
+	
+	def user
+		return object.user.username
+	end
 end
