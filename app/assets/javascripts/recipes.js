68cecf260948269d361a60f9b99a46e2c7a6b212
Monika <monika.m.williams@gmail.com>
2017-12-30 11:59:46 +0900

    started to build out next button, currently not working due to multiple issues

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,9 +1,23 @@
 $(function () {
-  $(".js-more").on('click', function(event) {
-		event.preventDefault();
+  $(".js-more").on('click', function() {
     var id = $(this).data("id");
     $.get("/recipes/" + id + ".json", function(data) {
       $("#description-" + id).text(data["description"]);
     });
   });
 });
+
+$(function (){
+	$(".js-next").on("click", function(){
+	var nextID = parseInt($(".js-next").attr("data-id")) + 1;
+	$.get("/recipes/" + nextID + ".json", function(data) {
+		$(".recipeImage").text(data["image"]);
+		$(".recipeName").text(data["name"]);
+		$(".recipeDescription").text(data["description"]);
+		$(".chefName").text(data["user"]["username"]);
+		$(".recipeIngredients").text(data["quantity"]["ingredient"]["name"]);
+		$(".recipeSteps").text(data["step"]);
+		$("js-next").attr("data-id", data["id"]);
+	});
+	});
+ });
