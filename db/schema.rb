483f2bfe521fe623ed83de6006a6f2aa2c99e82a
Monika <monika.m.williams@gmail.com>
2018-01-07 16:01:15 +0900

    added user bio attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225105658) do
+ActiveRecord::Schema.define(version: 20180107065239) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -71,6 +71,11 @@ ActiveRecord::Schema.define(version: 20171225105658) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
