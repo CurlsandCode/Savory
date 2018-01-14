5dc9e950f4e919c86caf2b0951e961d123092ae6
Monika <monika.m.williams@gmail.com>
2018-01-14 22:38:44 +0800

    trying to figure out why session isn't loading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
- 
+ skip_before_action :verify_authenticity_token
 	def show
-   @user = User.find_by(params[:id])
+		binding.pry
+		session[:init] = true
+   @user = current_user
 		@recipes = @user.recipes
     respond_to do |f|
       f.html { render :show}
